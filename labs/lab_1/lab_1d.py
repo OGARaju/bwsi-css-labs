c9188749542e5cb8282f9dfc05d38dbe27b79ea1
ogaraju <aparnaraju710@gmail.com>
2026-02-22 17:00:54 -0600

    add fix lab1d, unit tests, coverage

--- labs/lab_1/lab_1d.py
+++ labs/lab_1/lab_1d.py
@@ -13,24 +13,21 @@ def two_sum(nums: list[int], target: int) -> list[int]:
         for i in nums:
             if (i + j) == target:
                 sum = i + j
-                sum = str(sum)
                 ind1 = nums.index(i)
-                ind1= str(ind1)
                 ind2 = nums.index(j)
-                ind2 = str(ind2)
-                i = str(i)
-                j = str(j)
-                print("Because nums[" + j + "] + nums[" + i + "] == " + sum + ", we return [" + j + ", " + i + "].")
-                result = "[" + ind2 + ", " + ind1 + "]"
+                print(f"Because nums[{j}] + nums[{i}] == {sum}, we return [{j}, {i}].")
+                result = [ind2, ind1]
                 return result
                 break
             else:
                 continue
+    return []  # In case there is no solution, though the problem guarantees one exists.
+
 
 # Example usage:
 def main():
     nums = [2, 7, 11, 15]
-    target = 13
+    target = 29
     result = two_sum(nums, target)
     print(f"Indices of the two numbers that add up to {target}: {result}")
 
